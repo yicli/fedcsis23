7ede98d2f7d3a2b570e53a7ea6a85f5b31582f5e
Yichao Li <yichaoli8@gmail.com>
2023-06-02 18:18:33 +0200

    aug2

--- scripts/csv_categorical.py
+++ scripts/csv_categorical.py
@@ -3,8 +3,8 @@ import pandas as pd
 import numpy as np
 import pickle
 
-dir1 = 'data/features/train_aug'
-dir2 = 'data/features/valid_aug'
+dir1 = 'data/features/train_aug2'
+dir2 = 'data/features/valid_aug2'
 path1 = [os.path.join(dir1, f) for f in os.listdir(dir1)]
 path2 = [os.path.join(dir2, f) for f in os.listdir(dir2)]
 paths = path1 + path2
@@ -19,8 +19,8 @@ for p in paths:
     csv_uniq = pd.concat([csv_uniq, csv])
 
 uniq_vals = csv_uniq.csv.values
-with open('data/features/csv_cats.pkl', 'wb') as f:
-    pickle.dump(uniq_vals, f)
+# with open('data/features/csv_cats.pkl', 'wb') as f:
+#     pickle.dump(uniq_vals, f)
 
 # coerce to categorical
 for p in paths:
